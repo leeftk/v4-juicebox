158f107f6894b87771ab9c07fda7d0260da0163a
Jango <me.jango@protonmail.com>
2025-11-03 22:17:55 -0300

    deploy script chains w addresses

--- script/DeployJBUniswapV4Hook.s.sol
+++ script/DeployJBUniswapV4Hook.s.sol
@@ -19,12 +19,38 @@ import {IUniswapV3Factory} from "../src/interfaces/IUniswapV3Factory.sol";
 /// @notice Script to deploy the JBUniswapV4Hook with multi-currency support
 contract DeployJBUniswapV4Hook is Script {
     // Default test addresses (override with environment variables)
-    address constant DEFAULT_JB_TOKENS = 0x1234567890123456789012345678901234567890;
-    address constant DEFAULT_JB_DIRECTORY = 0x2345678901234567890123456789012345678901;
-    address constant DEFAULT_JB_CONTROLLER = 0x3456789012345678901234567890123456789012;
-    address constant DEFAULT_JB_PRICES = 0x4567890123456789012345678901234567890123;
-    address constant DEFAULT_JB_TERMINAL_STORE = 0x5678901234567890123456789012345678901234;
-    address constant DEFAULT_V3_FACTORY = address(0); // v3 factory disabled for now
+    address constant DEFAULT_JB_TOKENS = 0x4d0edd347fb1fa21589c1e109b3474924be87636;
+    address constant DEFAULT_JB_DIRECTORY = 0x0061e516886a0540f63157f112c0588ee0651dcf;
+    address constant DEFAULT_JB_PRICES = 0x9b90e507cf6b7eb681a506b111f6f50245e614c4;
+    address constant DEFAULT_JB_TERMINAL_STORE = 0xfe33b439ec53748c87dcedacb83f05add5014744;
+    address factory = address(0); 
+
+    if (block.chainid == 1) {
+        factory = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
+        // Ethereum Sepolia
+    } else if (block.chainid == 11_155_111) {
+        factory = 0x0227628f3F023bb0B980b67D528571c95c6DaC1c;
+        // Optimism Mainnet
+    } else if (block.chainid == 10) {
+        factory = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
+        // Base Mainnet
+    } else if (block.chainid == 8453) {
+        factory = 0x33128a8fC17869897dcE68Ed026d694621f6FDfD;
+        // Optimism Sepolia
+    } else if (block.chainid == 11_155_420) {
+        factory = 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24;
+        // BASE Sepolia
+    } else if (block.chainid == 84_532) {
+        factory = 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24;
+        // Arbitrum Mainnet
+    } else if (block.chainid == 42_161) {
+        factory = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
+        // Arbitrum Sepolia
+    } else if (block.chainid == 421_614) {
+        factory = 0x248AB79Bbb9bC29bB72f7Cd42F17e054Fc40188e;
+    } else {
+        revert("Invalid RPC / no juice contracts deployed on this network");
+    }
 
     function run() external {
         // Get the pool manager address from environment or use a default
@@ -34,10 +60,9 @@ contract DeployJBUniswapV4Hook is Script {
         // Get Juicebox addresses from environment or use defaults
         address jbTokens = vm.envOr("JB_TOKENS", DEFAULT_JB_TOKENS);
         address jbDirectory = vm.envOr("JB_DIRECTORY", DEFAULT_JB_DIRECTORY);
-        address jbController = vm.envOr("JB_CONTROLLER", DEFAULT_JB_CONTROLLER);
         address jbPrices = vm.envOr("JB_PRICES", DEFAULT_JB_PRICES);
         address jbTerminalStore = vm.envOr("JB_TERMINAL_STORE", DEFAULT_JB_TERMINAL_STORE);
-        address v3Factory = vm.envOr("V3_FACTORY", DEFAULT_V3_FACTORY);
+        address v3Factory = vm.envOr("V3_FACTORY", factory);
 
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address deployer = vm.addr(deployerPrivateKey);
@@ -47,7 +72,6 @@ contract DeployJBUniswapV4Hook is Script {
         console2.log("  Pool Manager:", poolManager);
         console2.log("  JB Tokens:", jbTokens);
         console2.log("  JB Directory:", jbDirectory);
-        console2.log("  JB Controller:", jbController);
         console2.log("  JB Prices:", jbPrices);
         console2.log("  JB Terminal Store:", jbTerminalStore);
 
@@ -79,7 +103,6 @@ contract DeployJBUniswapV4Hook is Script {
             IPoolManager(poolManager),
             IJBTokens(jbTokens),
             IJBDirectory(jbDirectory),
-            IJBController(jbController),
             IJBPrices(jbPrices),
             IJBTerminalStore(jbTerminalStore),
             IUniswapV3Factory(v3Factory)
@@ -99,7 +122,6 @@ contract DeployJBUniswapV4Hook is Script {
             IPoolManager(poolManager),
             IJBTokens(jbTokens),
             IJBDirectory(jbDirectory),
-            IJBController(jbController),
             IJBPrices(jbPrices),
             IJBTerminalStore(jbTerminalStore),
             IUniswapV3Factory(v3Factory)
